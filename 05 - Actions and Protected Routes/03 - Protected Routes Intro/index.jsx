806846b96f7a45a85c2dd194b2b2057bac00bb94
tanaliga100 <jordantanaliga@gmail.com>
2023-05-17 18:04:14 +0800

    actions-and-protected-routes

--- 05 - Actions and Protected Routes/03 - Protected Routes Intro/index.jsx
+++ 05 - Actions and Protected Routes/03 - Protected Routes Intro/index.jsx
@@ -1,25 +1,24 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
+import React from "react";
+import ReactDOM from "react-dom/client";
 import {
+  Route,
   RouterProvider,
   createBrowserRouter,
   createRoutesFromElements,
-  Route
-} from "react-router-dom"
+} from "react-router-dom";
 
-import Layout from "./Layout"
+import Layout from "./Layout";
 
-const router = createBrowserRouter(createRoutesFromElements(
-  <Route path="/" element={<Layout />}>
-    <Route index element={<h1>Home page</h1>} />
-    <Route path="protected" element={<h1>Super secret info here</h1>} />
-  </Route>
-))
-
-function App() {
-  return (
-    <RouterProvider router={router} />
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route index element={<h1>Home page</h1>} />
+      <Route path="protected" element={<h1>Super secret info here</h1>} />
+    </Route>
   )
+);
+function App() {
+  return <RouterProvider router={router} />;
 }
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />)
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
