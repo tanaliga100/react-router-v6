0ff9dffd1a98c5c89f81e18d557f435e7830c48f
tanaliga100 <jordantanaliga@gmail.com>
2023-05-17 14:24:46 +0800

    search-params-links

--- 03 - Search Params and Links/10 - Caveats to setting params/index.jsx
+++ 03 - Search Params and Links/10 - Caveats to setting params/index.jsx
@@ -1,34 +1,39 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route, Link, useSearchParams } from "react-router-dom";
+import {
+  BrowserRouter,
+  Link,
+  Route,
+  Routes,
+  useSearchParams,
+} from "react-router-dom";
 
 const swCharacters = [
   { name: "Luke Skywalker", type: "Jedi" },
   { name: "Darth Vader", type: "Sith" },
   { name: "Emperor Palpatine", type: "Sith" },
-  { name: "Yoda", type: "Jedi" }
-]
+  { name: "Yoda", type: "Jedi" },
+];
 
 function HomePage() {
-  const [searchParams, setSearchParams] = useSearchParams()
-  const typeFilter = searchParams.get("type")
+  const [searchParams, setSearchParams] = useSearchParams();
+  const typeFilter = searchParams.get("type");
 
   const displayedCharacters = typeFilter
-    ? swCharacters.filter(char => char.type.toLowerCase() === typeFilter)
-    : swCharacters
+    ? swCharacters.filter((char) => char.type.toLowerCase() === typeFilter)
+    : swCharacters;
 
-  const charEls = displayedCharacters
-    .map(char => (
-      <div key={char.name}>
-        <h3
-          style={{ color: char.type.toLowerCase() === "jedi" ? "blue" : "red" }}
-        >
-          Name: {char.name}
-        </h3>
-        <p>Type: {char.type}</p>
-        <hr />
-      </div>
-    ))
+  const charEls = displayedCharacters.map((char) => (
+    <div key={char.name}>
+      <h3
+        style={{ color: char.type.toLowerCase() === "jedi" ? "blue" : "red" }}
+      >
+        Name: {char.name}
+      </h3>
+      <p>Type: {char.type}</p>
+      <hr />
+    </div>
+  ));
 
   return (
     <main>
@@ -56,7 +61,7 @@ function App() {
         <Route path="/characters" element={<HomePage />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />)
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
