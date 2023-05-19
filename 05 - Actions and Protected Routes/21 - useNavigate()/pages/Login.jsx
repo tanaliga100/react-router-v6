399d8f8994d5584d9900db0e9bd785ef70ba2f71
tanaliga100 <jordantanaliga@gmail.com>
2023-05-19 15:45:33 +0800

    delete-the-native-form-submission

--- 05 - Actions and Protected Routes/21 - useNavigate()/pages/Login.jsx
+++ 05 - Actions and Protected Routes/21 - useNavigate()/pages/Login.jsx
@@ -1,65 +1,68 @@
-import React from "react"
-import { useLoaderData } from "react-router-dom"
-import { loginUser } from "../api"
+import React from "react";
+import { useLoaderData, useNavigate } from "react-router-dom";
+import { loginUser } from "../api";
 
 export function loader({ request }) {
-    return new URL(request.url).searchParams.get("message")
+  return new URL(request.url).searchParams.get("message");
 }
 
 export default function Login() {
-    const [loginFormData, setLoginFormData] = React.useState({ email: "", password: "" })
-    const [status, setStatus] = React.useState("idle")
-    const [error, setError] = React.useState(null)
-    const message = useLoaderData()
+  const [loginFormData, setLoginFormData] = React.useState({
+    email: "",
+    password: "",
+  });
+  const [status, setStatus] = React.useState("idle");
+  const [error, setError] = React.useState(null);
+  const message = useLoaderData();
 
-    function handleSubmit(e) {
-        e.preventDefault()
-        setStatus("submitting")
-        setError(null)
-        loginUser(loginFormData)
-            .then(data => console.log(data))
-            .catch(err => setError(err))
-            .finally(() => setStatus("idle"))
-    }
+  const navigate = useNavigate();
 
-    function handleChange(e) {
-        const { name, value } = e.target
-        setLoginFormData(prev => ({
-            ...prev,
-            [name]: value
-        }))
-    }
+  function handleSubmit(e) {
+    e.preventDefault();
+    setStatus("submitting");
+    setError(null);
+    loginUser(loginFormData)
+      .then((data) => {
+        console.log({ data });
+        navigate("/host", { replace: true });
+      })
+      .catch((err) => setError(err))
+      .finally(() => setStatus("idle"));
+  }
 
-    return (
-        <div className="login-container">
-            <h1>Sign in to your account</h1>
-            {message && <h3 className="red">{message}</h3>}
-            {error && <h3 className="red">{error.message}</h3>}
+  function handleChange(e) {
+    const { name, value } = e.target;
+    setLoginFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  }
 
-            <form onSubmit={handleSubmit} className="login-form">
-                <input
-                    name="email"
-                    onChange={handleChange}
-                    type="email"
-                    placeholder="Email address"
-                    value={loginFormData.email}
-                />
-                <input
-                    name="password"
-                    onChange={handleChange}
-                    type="password"
-                    placeholder="Password"
-                    value={loginFormData.password}
-                />
-                <button 
-                    disabled={status === "submitting"}
-                >
-                    {status === "submitting" 
-                        ? "Logging in..." 
-                        : "Log in"
-                    }
-                </button>
-            </form>
-        </div>
-    )
+  return (
+    <div className="login-container">
+      <h1>Sign in to your account</h1>
+      {message && <h3 className="red">{message}</h3>}
+      {error && <h3 className="red">{error.message}</h3>}
+
+      <form onSubmit={handleSubmit} className="login-form">
+        <input
+          name="email"
+          onChange={handleChange}
+          type="email"
+          placeholder="Email address"
+          value={loginFormData.email}
+        />
+        <input
+          name="password"
+          onChange={handleChange}
+          type="password"
+          placeholder="Password"
+          value={loginFormData.password}
+        />
+        <button disabled={status === "submitting"}>
+          {status === "submitting" ? "Logging in..." : "Log in"}
+        </button>
+      </form>
+    </div>
+  );
 }
--- 05 - Actions and Protected Routes/22 - React Router Form Component/Layout.jsx
+++ 05 - Actions and Protected Routes/22 - React Router Form Component/Layout.jsx
@@ -1,17 +1,18 @@
-import React from "react"
-import { Outlet, Link } from "react-router-dom"
+import React from "react";
+import { Link, Outlet } from "react-router-dom";
 
 export default function Layout() {
   return (
     <>
-    <nav>
-      <Link to="/">Home</Link>
-      <Link to="protected">Protected</Link>
-      <Link to="login">Login</Link>
-    </nav>
-    <main>
-      <Outlet />
-    </main>
+      <nav>
+        <Link to="/">Home</Link>
+
+        <Link to="protected">Protected</Link>
+        <Link to="login">Login</Link>
+      </nav>
+      <main>
+        <Outlet />
+      </main>
     </>
   );
 }
